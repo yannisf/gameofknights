2b4f24c9e2b2f02abfb1c6d786ecaf1e11a80555
Yannis Frangos <yanfrangos@protonmail.com>
2023-02-15 21:30:50 +0200

    Small refactor

--- src/main/scala/example/GameOfKnights.scala
+++ src/main/scala/example/GameOfKnights.scala
@@ -8,35 +8,33 @@ object GameOfKnights extends App {
   val numberOfKnights = 5
   val initialHealth = 10
 
-  val knights = (1 to numberOfKnights).map(i => Knight(s"Knight_${i}"))
+  val knights = (1 to numberOfKnights).map(i => Knight(s"Knight_${i}")) //Initialize knights
 
   case class Knight(name: String, health: Int = initialHealth) {
-    def attack() = Random.nextInt(6) + 1
-    def isDead = health <= 0
+    def attack(): Int = Random.nextInt(6) + 1
+    def isDead: Boolean = health <= 0
   }
 
   @tailrec
-  def recurse(knights: Seq[Knight]): Seq[Knight] = {
-    if (knights.size == 1) knights
+  private def fight(knights: Seq[Knight]): Seq[Knight] = {
+    if (knights.size == 1) knights //Termination condition
     else {
       val newKnights = knights match {
-        case Seq(attacker, defender, tail @ _*) =>
+        case Seq(attacker, defender, tail@_*) =>
           val attackPoints = attacker.attack()
           val newDefender = defender.copy(health = defender.health - attackPoints)
           println(s"${attacker.name} deals $attackPoints damage to ${defender.name}")
           if (newDefender.isDead) {
             println(s"Knight ${newDefender.name} has died...")
-            tail ++ Seq(attacker)
-          } else {
-            Seq(newDefender) ++ tail ++ Seq (attacker)
-          }
+            tail :+ attacker //Defender is removed from the list, the attacker, is moved to the end
+          } else (newDefender +: tail) :+ attacker //Defender takes head, attacher is moved to the end
       }
       Thread.sleep(100L)
-      recurse(newKnights)
+      fight(newKnights)
     }
   }
 
-  println(s"${recurse(knights).head.name} stands victorious!")
+  println(s"${fight(knights).head.name} stands victorious!")
 
 }
 
