b768e8eba561ab585218c3a829101ed200188bc5
Yannis Frangos <yanfrangos@protonmail.com>
2023-02-16 09:17:54 +0200

    Why use if when you can pattern match!

--- src/main/scala/example/GameOfKnights.scala
+++ src/main/scala/example/GameOfKnights.scala
@@ -5,36 +5,29 @@ import scala.util.Random
 
 object GameOfKnights extends App {
 
-  val numberOfKnights = 5
-  val initialHealth = 10
-
-  val knights = (1 to numberOfKnights).map(i => Knight(s"Knight_$i")) //Initialize knights
+  val numberOfKnights = 3
+  val initialHealth = 6
 
   case class Knight(name: String, health: Int = initialHealth) {
     def attack(): Int = Random.nextInt(6) + 1
     def isDead: Boolean = health <= 0
   }
 
+  val knights = (1 to numberOfKnights).map(i => Knight(s"Knight_$i")) //Initialize knights
+
   @tailrec
-  private def fight(knights: Seq[Knight]): Seq[Knight] = {
-    if (knights.size == 1) knights //Termination condition
-    else {
-      val newKnights = knights match {
-        case Seq(attacker, defender, tail@_*) => //Pattern match decomposes list to its elements
-          val attackPoints = attacker.attack()
-          val newDefender = defender.copy(health = defender.health - attackPoints)
-          println(s"${attacker.name} deals $attackPoints damage to ${defender.name}")
-          if (newDefender.isDead) {
-            println(s"${newDefender.name} has died...")
-            tail :+ attacker //Defender is removed from the list, the attacker, is moved to the end
-          } else (newDefender +: tail) :+ attacker //Defender takes head, attacher is moved to the end
-      }
-      Thread.sleep(100L)
-      fight(newKnights)
-    }
+  private def fight(knights: Seq[Knight] = knights): Seq[Knight] = knights match {
+    case Seq(_) => knights //Termination condition
+    case Seq(attacker, defender, tail@_*) => //Pattern match decomposes list to its elements
+      val newDefender = defender.copy(health = defender.health - attacker.attack())
+      println(s"${attacker.name} deals ${defender.health - newDefender.health} damage to ${defender.name}")
+      if (newDefender.isDead) {
+        println(s"${newDefender.name} has died...")
+        fight(tail :+ attacker) //Defender is removed, the attacker is moved to the end, and let the next round begin
+      } else fight(newDefender +: tail :+ attacker) //Defender takes head, attacker is moved to the end, and let the next round begin
   }
 
-  println(s"${fight(knights).head.name} stands victorious!")
+  println(s"${fight().head.name} stands victorious!")
 
 }
 
